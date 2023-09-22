5e490a4100b5f2dc8f850386599694210c4d4191
luoxinliang <luoxinliang@xjgreat.com>
2023-09-22 10:16:01 +0800

    Mutiple streams merge into a single stream.

--- seatunnel-core/seatunnel-flink-starter/seatunnel-flink-starter-common/src/main/java/org/apache/seatunnel/core/starter/flink/execution/SinkExecuteProcessor.java
+++ seatunnel-core/seatunnel-flink-starter/seatunnel-flink-starter-common/src/main/java/org/apache/seatunnel/core/starter/flink/execution/SinkExecuteProcessor.java
@@ -102,25 +102,28 @@ public class SinkExecuteProcessor
     @Override
     public List<DataStream<Row>> execute(List<DataStream<Row>> upstreamDataStreams)
             throws TaskExecuteException {
-        DataStream<Row> input = upstreamDataStreams.get(0);
         for (int i = 0; i < plugins.size(); i++) {
             Config sinkConfig = pluginConfigs.get(i);
-            SeaTunnelSink<SeaTunnelRow, Serializable, Serializable, Serializable> seaTunnelSink =
-                    plugins.get(i);
-            DataStream<Row> stream = fromSourceTable(sinkConfig, i).orElse(input);
-            seaTunnelSink.setTypeInfo(
-                    (SeaTunnelRowType) TypeConverterUtils.convert(stream.getType()));
-            if (SupportDataSaveMode.class.isAssignableFrom(seaTunnelSink.getClass())) {
-                SupportDataSaveMode saveModeSink = (SupportDataSaveMode) seaTunnelSink;
-                DataSaveMode dataSaveMode = saveModeSink.getDataSaveMode();
-                saveModeSink.handleSaveMode(dataSaveMode);
-            }
-            DataStreamSink<Row> dataStreamSink =
-                    stream.sinkTo(SinkV1Adapter.wrap(new FlinkSink<>(seaTunnelSink)))
-                            .name(seaTunnelSink.getPluginName());
-            if (sinkConfig.hasPath(CommonOptions.PARALLELISM.key())) {
-                int parallelism = sinkConfig.getInt(CommonOptions.PARALLELISM.key());
-                dataStreamSink.setParallelism(parallelism);
+            List<String> stringList = sinkConfig.getStringList(SOURCE_TABLE_NAME);
+            for (int ii = 0; ii < stringList.size(); ii++) {
+                DataStream<Row> input = upstreamDataStreams.get(ii);
+                SeaTunnelSink<SeaTunnelRow, Serializable, Serializable, Serializable> seaTunnelSink =
+                        plugins.get(i);
+                DataStream<Row> stream = fromSourceTable(sinkConfig, ii).orElse(input);
+                seaTunnelSink.setTypeInfo(
+                        (SeaTunnelRowType) TypeConverterUtils.convert(stream.getType()));
+                if (SupportDataSaveMode.class.isAssignableFrom(seaTunnelSink.getClass())) {
+                    SupportDataSaveMode saveModeSink = (SupportDataSaveMode) seaTunnelSink;
+                    DataSaveMode dataSaveMode = saveModeSink.getDataSaveMode();
+                    saveModeSink.handleSaveMode(dataSaveMode);
+                }
+                DataStreamSink<Row> dataStreamSink =
+                        stream.sinkTo(new FlinkSink<>(seaTunnelSink))
+                                .name(seaTunnelSink.getPluginName());
+                if (sinkConfig.hasPath(CommonOptions.PARALLELISM.key())) {
+                    int parallelism = sinkConfig.getInt(CommonOptions.PARALLELISM.key());
+                    dataStreamSink.setParallelism(parallelism);
+                }
             }
         }
         // the sink is the last stream
